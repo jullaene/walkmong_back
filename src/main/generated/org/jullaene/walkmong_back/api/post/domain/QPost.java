ea5baf5ac4030a033c37b743a18ffd2207d255b0
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-03 04:39:14 +0900

    feat: querydsl 설정 및 도메인 오류 수정

---
+++ src/main/generated/org/jullaene/walkmong_back/api/post/domain/QPost.java
@@ -0,0 +1,72 @@
+package org.jullaene.walkmong_back.api.post.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = -294090118L;
+
+    public static final QPost post = new QPost("post");
+
+    public final org.jullaene.walkmong_back.common.QBaseEntity _super = new org.jullaene.walkmong_back.common.QBaseEntity(this);
+
+    public final StringPath addressMemo = createString("addressMemo");
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath delYn = _super.delYn;
+
+    public final NumberPath<Long> dogId = createNumber("dogId", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> endTime = createDateTime("endTime", java.time.LocalDateTime.class);
+
+    public final StringPath locationNegotiableYn = createString("locationNegotiableYn");
+
+    public final StringPath matchingYn = createString("matchingYn");
+
+    public final StringPath memo = createString("memo");
+
+    public final NumberPath<Long> ownerAddressId = createNumber("ownerAddressId", Long.class);
+
+    public final NumberPath<Long> postId = createNumber("postId", Long.class);
+
+    public final StringPath preMeetAvailableYn = createString("preMeetAvailableYn");
+
+    public final DateTimePath<java.time.LocalDateTime> startTime = createDateTime("startTime", java.time.LocalDateTime.class);
+
+    public final StringPath suppliesProvideYn = createString("suppliesProvideYn");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final EnumPath<org.jullaene.walkmong_back.api.post.domain.enums.WalkingStatus> walkingStatus = createEnum("walkingStatus", org.jullaene.walkmong_back.api.post.domain.enums.WalkingStatus.class);
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+
--- src/main/java/org/jullaene/walkmong_back/api/post/service/PostService.java
+++ src/main/java/org/jullaene/walkmong_back/api/post/service/PostService.java
@@ -1,10 +1,11 @@
 package org.jullaene.walkmong_back.api.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.jullaene.walkmong_back.api.post.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
-    private final PostService postService;
+    private final PostRepository postRepository;
 }
