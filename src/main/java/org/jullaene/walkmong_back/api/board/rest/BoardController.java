7cf4ffb5351fcc62c78a97bee2ea008b30b01ff9
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-14 23:46:19 +0900

    refactor: Post 이름으로 지정되어 있던 파일들 모두 Board로 변경

--- src/main/java/org/jullaene/walkmong_back/api/board/rest/BoardController.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/rest/BoardController.java
@@ -2,14 +2,14 @@ package org.jullaene.walkmong_back.api.board.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.jullaene.walkmong_back.api.board.service.PostService;
+import org.jullaene.walkmong_back.api.board.service.BoardService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Post", description = "게시글 정보 관련 api 입니다.")
+@Tag(name = "Board", description = "게시글 정보 관련 api 입니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/post")
-public class PostController {
-    private final PostService postService;
+@RequestMapping("/api/v1/board")
+public class BoardController {
+    private final BoardService boardService;
 }
--- src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
@@ -1,11 +1,11 @@
 package org.jullaene.walkmong_back.api.board.service;
 
 import lombok.RequiredArgsConstructor;
-import org.jullaene.walkmong_back.api.board.repository.PostRepository;
+import org.jullaene.walkmong_back.api.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PostService {
-    private final PostRepository postRepository;
+public class BoardService {
+    private final BoardRepository boardRepository;
 }
