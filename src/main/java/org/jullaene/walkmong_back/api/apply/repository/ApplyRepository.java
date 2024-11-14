f265d79a761745b3ea5d1de4a1ac7e8102507f94
dnwldla <leeemjw@naver.com>
2024-11-14 13:42:45 +0900

    refactor: 도메인 이름 수정 도메인 이름을 request에서 apply로 수정

---
+++ src/main/java/org/jullaene/walkmong_back/api/apply/repository/ApplyRepository.java
@@ -0,0 +1,9 @@
+package org.jullaene.walkmong_back.api.apply.repository;
+
+import org.jullaene.walkmong_back.api.apply.domain.Apply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplyRepository extends JpaRepository<Apply, Long> {
+}
