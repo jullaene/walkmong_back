cec608531718cd962a7b01417b561a684eccfc80
dnwldla <leeemjw@naver.com>
2024-11-15 18:05:12 +0900

    feat: #5 산책 지원 기능 구현

--- src/main/java/org/jullaene/walkmong_back/api/apply/rest/ApplyController.java
+++ src/main/java/org/jullaene/walkmong_back/api/apply/rest/ApplyController.java
@@ -1,7 +1,12 @@
 package org.jullaene.walkmong_back.api.apply.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.jullaene.walkmong_back.api.apply.dto.req.ApplyRequestDto;
 import org.jullaene.walkmong_back.api.apply.service.ApplyService;
+import org.jullaene.walkmong_back.common.BasicResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/apply")
 public class ApplyController {
     private final ApplyService applyService;
+
+    @PostMapping("/{boardId}")
+    public ResponseEntity<BasicResponse<Long>> saveApply(@RequestBody ApplyRequestDto applyRequestDto){
+        applyService.saveApply(applyRequestDto);
+
+        return ResponseEntity.ok(BasicResponse.ofCreateSuccess(applyService.saveApply(applyRequestDto)));
+    }
 }
