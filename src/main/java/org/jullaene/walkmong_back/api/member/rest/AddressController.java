4aa4abed50a2498fc1d540c43296eb552caabd9e
Seung Ik Lee <natwo98@gmail.com>
2024-11-16 02:30:58 +0900

    feat: #4 산책자 주소 리스트 조회 기능 구현

--- src/main/java/org/jullaene/walkmong_back/api/member/rest/AddressController.java
+++ src/main/java/org/jullaene/walkmong_back/api/member/rest/AddressController.java
@@ -1,8 +1,15 @@
 package org.jullaene.walkmong_back.api.member.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.jullaene.walkmong_back.api.member.domain.Member;
+import org.jullaene.walkmong_back.api.member.dto.res.AddressResponseDto;
 import org.jullaene.walkmong_back.api.member.service.AddressService;
+import org.jullaene.walkmong_back.api.member.service.MemberService;
+import org.jullaene.walkmong_back.common.BasicResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/address")
 public class AddressController {
     private final AddressService addressService;
+    private final MemberService memberService;
+
+    @GetMapping("/list")
+    public ResponseEntity<BasicResponse<List<AddressResponseDto>>> getAddressList() {
+        Member member = memberService.getMemberFromUserDetail();
+        return ResponseEntity.ok(BasicResponse.ofSuccess(addressService.getAddresses(member.getMemberId())));
+    }
 }
