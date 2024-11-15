8059b0199147bd1d3c3d916ea246405af628ca3d
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-15 22:01:06 +0900

    feat: 게시글 리스트 조회 api 구현[#2]

--- src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
@@ -1,7 +1,7 @@
 package org.jullaene.walkmong_back.api.board.service;
 
 import lombok.RequiredArgsConstructor;
-import org.jullaene.walkmong_back.api.board.dto.res.BoardRes;
+import org.jullaene.walkmong_back.api.board.dto.res.BoardResponseDto;
 import org.jullaene.walkmong_back.api.board.repository.BoardRepository;
 import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
 import org.jullaene.walkmong_back.api.member.domain.Address;
@@ -12,7 +12,6 @@ import org.jullaene.walkmong_back.api.member.service.MemberService;
 import org.jullaene.walkmong_back.common.exception.CustomException;
 import org.jullaene.walkmong_back.common.exception.ErrorType;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -28,20 +27,35 @@ public class BoardService {
     /**
      * 게시글 리스트 조회
      * */
-    public List<BoardRes> getBoards(LocalDate date, Long addressId, DistanceRange distance, DogSize dogSize, String matchingYn) {
+    public List<BoardResponseDto> getBoards(LocalDate date, Long addressId, DistanceRange distance, DogSize dogSize, String matchingYn) {
         Member member = memberService.getMemberFromUserDetail();
 
         // 주소 id가 null이면 기본 주소 반환, 주소 id가 null이 아니면 해당 id를 가진 주소 반환
         Address address;
         if (addressId == null) {
-            address = addressRepository.findByMemberIdAndBasicAddressYn(member.getMemberId(), "Y")
-                    .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, ErrorType.INVALID_ADDRESS));
-        }
+            address = getBasicAddressAndDelYn(member.getMemberId(), "N");
+            }
         else {
-            address = addressRepository.findById(addressId)
-                    .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, ErrorType.INVALID_ADDRESS));
+            address = getAddressByIdAndDelYn(addressId, "Y");
         }
 
         return boardRepository.getBoardsWithFilters(date, address, distance, dogSize, matchingYn);
+    }
+
+    /**
+     * 주어진 멤버가 가진 기본 address를 반환
+     * */
+    private Address getBasicAddressAndDelYn(Long memberId, String delYn) {
+        return addressRepository.findByMemberIdAndBasicAddressYnAndDelYn(memberId,"Y", delYn)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, ErrorType.INVALID_ADDRESS));
+    }
+
+    /**
+     * 주어진 addressId를 이용하여 address 반환
+     * */
+    private Address getAddressByIdAndDelYn (Long addressId, String delYn) {
+        return addressRepository.findByAddressIdAndDelYn(addressId, delYn)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, ErrorType.INVALID_ADDRESS));
+
     }
 }
