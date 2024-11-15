155423e956bd84e6c2dc3d17789abc4351ece2ae
Seung Ik Lee <natwo98@gmail.com>
2024-11-15 15:20:15 +0900

    feat: #6 반려견 프로필 상세 조회 기능 구현

--- src/main/java/org/jullaene/walkmong_back/api/dog/service/DogService.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/service/DogService.java
@@ -1,11 +1,23 @@
 package org.jullaene.walkmong_back.api.dog.service;
 
 import lombok.RequiredArgsConstructor;
+import org.jullaene.walkmong_back.api.dog.domain.Dog;
+import org.jullaene.walkmong_back.api.dog.dto.res.DogProfileResponseDto;
 import org.jullaene.walkmong_back.api.dog.repository.DogRepository;
+import org.jullaene.walkmong_back.common.exception.CustomException;
+import org.jullaene.walkmong_back.common.exception.ErrorType;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class DogService {
     private final DogRepository dogRepository;
+
+    public DogProfileResponseDto getDogProfile(Long dogId) {
+        Dog dog = dogRepository.findById(dogId)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, ErrorType.DOG_NOT_FOUND));
+
+        return DogProfileResponseDto.fromDog(dog);
+    }
 }
