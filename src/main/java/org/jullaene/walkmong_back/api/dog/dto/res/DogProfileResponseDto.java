5990b8da0fc9f1bc8985acfc15138e2cbf484d7e
Seung Ik Lee <natwo98@gmail.com>
2024-11-15 23:14:16 +0900

    refactor: #6 코드 리뷰 반영

--- src/main/java/org/jullaene/walkmong_back/api/dog/dto/res/DogProfileResponseDto.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/dto/res/DogProfileResponseDto.java
@@ -1,9 +1,7 @@
 package org.jullaene.walkmong_back.api.dog.dto.res;
 
-import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
-import org.jullaene.walkmong_back.api.dog.domain.Dog;
 import org.jullaene.walkmong_back.common.enums.Gender;
 
 @Getter
@@ -21,24 +19,4 @@ public class DogProfileResponseDto {
     private String friendly;
     private String barking;
     private String rabiesYn;
-
-    public static DogProfileResponseDto fromDog(Dog dog) {
-        int currentYear = LocalDate.now().getYear();
-        int dogAge = currentYear - dog.getBirthYear() + 1;
-
-        return DogProfileResponseDto.builder()
-                .dogId(dog.getDogId())
-                .dogName(dog.getName())
-                .dogProfile(dog.getProfile())
-                .dogGender(dog.getGender())
-                .dogAge(dogAge)
-                .breed(dog.getBreed())
-                .weight(dog.getWeight())
-                .neuteringYn(dog.getNeuteringYn())
-                .bite(dog.getBite())
-                .friendly(dog.getFriendly())
-                .barking(dog.getBarking())
-                .rabiesYn(dog.getRabiesYn())
-                .build();
-    }
 }
--- src/main/java/org/jullaene/walkmong_back/api/dog/rest/DogController.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/rest/DogController.java
@@ -3,8 +3,8 @@ package org.jullaene.walkmong_back.api.dog.rest;
 import lombok.RequiredArgsConstructor;
 import org.jullaene.walkmong_back.api.dog.dto.res.DogProfileResponseDto;
 import org.jullaene.walkmong_back.api.dog.service.DogService;
-import org.jullaene.walkmong_back.common.exception.ResponseMessage;
-import org.springframework.http.HttpStatus;
+import org.jullaene.walkmong_back.common.BasicResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +17,8 @@ public class DogController {
     private final DogService dogService;
 
     @GetMapping("/profile/{dogId}")
-    public ResponseMessage<DogProfileResponseDto> getDogProfile(@PathVariable Long dogId) {
+    public ResponseEntity<BasicResponse<DogProfileResponseDto>> getDogProfile(@PathVariable Long dogId) {
         DogProfileResponseDto dogProfile = dogService.getDogProfile(dogId);
-        return ResponseMessage.of(HttpStatus.OK, "Success", dogProfile);
+        return ResponseEntity.ok(BasicResponse.ofSuccess(dogProfile));
     }
 }
