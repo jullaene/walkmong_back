8dd4cce8a191231cb33c144d7c9e60fa9961de4e
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-16 02:23:30 +0900

    fix: 산책 지원 api 오류 수정 [#12]
    산책 지원 api에서 boardId와 memoToOwner 저장 안되는 문제 해결
    memberId 연결
    본인이 쓴 게시글에는 지원 불가, 한 게시글에는 여러번 지원 불가 에러 추가

---
+++ src/main/java/org/jullaene/walkmong_back/api/board/repository/BoardRepositoryCustom.java
@@ -0,0 +1,14 @@
+package org.jullaene.walkmong_back.api.board.repository;
+
+import org.jullaene.walkmong_back.api.board.dto.res.BoardResponseDto;
+import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
+import org.jullaene.walkmong_back.api.member.domain.Address;
+import org.jullaene.walkmong_back.api.member.domain.enums.DistanceRange;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface BoardRepositoryCustom {
+    List<BoardResponseDto> getBoardsWithFilters(LocalDate date, Address walkerAddress, DistanceRange distance, DogSize dogSize, String matchingYn);
+    boolean existsByBoardIdAndMemberIdAndDelYn(Long boardId, Long memberId, String delYn);
+}
---
+++ src/main/java/org/jullaene/walkmong_back/api/board/repository/impl/BoardRepositoryImpl.java
@@ -0,0 +1,138 @@
+package org.jullaene.walkmong_back.api.board.repository.impl;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
+import com.querydsl.core.types.dsl.StringTemplate;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.jullaene.walkmong_back.api.board.domain.QBoard;
+import org.jullaene.walkmong_back.api.board.dto.res.BoardResponseDto;
+import org.jullaene.walkmong_back.api.board.repository.BoardRepositoryCustom;
+import org.jullaene.walkmong_back.api.dog.domain.QDog;
+import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
+import org.jullaene.walkmong_back.api.member.domain.Address;
+import org.jullaene.walkmong_back.api.member.domain.QAddress;
+import org.jullaene.walkmong_back.api.member.domain.enums.DistanceRange;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+public class BoardRepositoryImpl implements BoardRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public BoardRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    /**
+     * 게시글 리스트 조회
+     *  date, walkerAddress, distance, dogSize, matchingYn을 기준으로 적용
+     * */
+    @Override
+    public List<BoardResponseDto> getBoardsWithFilters(LocalDate date, Address walkerAddress, DistanceRange distance, DogSize dogSize, String matchingYn) {
+        QBoard board = QBoard.board;
+        QDog dog = QDog.dog;
+        QAddress ownerAddress = QAddress.address;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // date가 null이면 당일을 조건에 삽입
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(23, 59, 59, 999_999_999); // 23:59:59.999999
+
+        builder.and(board.startTime.between(startOfDay, endOfDay));
+
+        if (dogSize != null) {
+            builder.and(board.dogId.in(
+                    JPAExpressions.select(dog.dogId)
+                            .from(dog)
+                            .where(dog.dogSize.eq(dogSize))));
+        }
+
+        if (matchingYn != null) {
+            builder.and(board.matchingYn.eq(matchingYn));
+        }
+
+        if (distance == null) {
+            distance = walkerAddress.getDistanceRange();
+        }
+
+        // 거리 계산
+        NumberTemplate<Double> distanceExpression = Expressions.numberTemplate(
+                Double.class,
+                "ST_Distance_Sphere(point({0}, {1}), point({2}, {3}))",
+                JPAExpressions.select(ownerAddress.longitude)
+                        .from(ownerAddress)
+                        .where(ownerAddress.addressId.eq(board.ownerAddressId)),
+                JPAExpressions.select(ownerAddress.latitude)
+                        .from(ownerAddress)
+                        .where(ownerAddress.addressId.eq(board.ownerAddressId)), walkerAddress.getLongitude(), walkerAddress.getLatitude()
+        );
+
+        BooleanExpression isWithinRange = distanceExpression.lt(distance.getRange());
+
+        // 거리 결과를 실제로 조회할 때
+        Double distanceResult = queryFactory.select(distanceExpression).from(board).fetchOne();
+        log.info("Calculated Distance: " + distanceResult);
+
+        StringTemplate startTimeExpression = Expressions.stringTemplate("DATE_FORMAT({0}, '%H:%i')", board.startTime);
+        StringTemplate endTimeExpression = Expressions.stringTemplate("DATE_FORMAT({0}, '%H:%i')", board.endTime);
+
+        return queryFactory.select(
+                        Projections.constructor(BoardResponseDto.class,
+                                startTimeExpression.as("startTime"),
+                                endTimeExpression.as("endTime"),
+                                board.matchingYn.as("matchingYn"),
+                                dog.name.as("dogName"),
+                                dog.profile.as("dogProfile"),
+                                dog.gender.as("dogGender"),
+                                dog.breed.as("breed"),
+                                dog.weight.as("weight"),
+                                dog.dogSize.as("dogSize"),
+                                board.content.as("content"),
+                                ownerAddress.dongAddress.as("dongAddress"),
+                                distanceExpression.as("distance")
+                        )
+                )
+                .from(board)
+                .leftJoin(dog).on(dog.dogId.eq(board.dogId))
+                .leftJoin(ownerAddress).on(ownerAddress.addressId.eq(board.ownerAddressId))
+                .where(builder)
+                .where(isWithinRange)
+                .orderBy(board.startTime.asc())
+                .fetch();
+    }
+
+    /**
+     * boardId와 memberId를 기준으로 해당 게시글의 작성자인지 확인
+     * */
+    @Override
+    public boolean existsByBoardIdAndMemberIdAndDelYn(Long boardId, Long memberId, String delYn) {
+        QBoard board = QBoard.board;
+        QDog dog = QDog.dog;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(board.boardId.eq(boardId));
+        builder.and(board.delYn.eq(delYn));
+        builder.and(dog.memberId.eq(memberId));
+
+        long count = queryFactory
+                .selectFrom(board)
+                .join(dog).on(board.dogId.eq(dog.dogId).and(dog.delYn.eq(delYn)))
+                .where(builder)
+                .fetch()
+                .size();
+
+        return count > 0;
+    }
+}
