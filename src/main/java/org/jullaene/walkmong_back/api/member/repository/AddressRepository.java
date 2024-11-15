2d05ac7208ef176be7da68d93274939db5db2158
Seung Ik Lee <natwo98@gmail.com>
2024-11-16 02:30:58 +0900

    feat: #4 산책자 주소 리스트 조회 기능 구현

--- src/main/java/org/jullaene/walkmong_back/api/member/repository/AddressRepository.java
+++ src/main/java/org/jullaene/walkmong_back/api/member/repository/AddressRepository.java
@@ -1,5 +1,6 @@
 package org.jullaene.walkmong_back.api.member.repository;
 
+import java.util.List;
 import org.jullaene.walkmong_back.api.member.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface AddressRepository extends JpaRepository<Address, Long> {
     Optional<Address> findByAddressIdAndDelYn(Long addressId, String delYn);
     Optional<Address> findByMemberIdAndBasicAddressYnAndDelYn(Long memberId, String basicAddressYn, String delYn);
+    List<Address> findByMemberIdAndDelYn(Long memberId, String delYn);
 }
