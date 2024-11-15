a7153bdb47663e8f3940c2707061e0da95e28601
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-16 03:55:48 +0900

    refactor: ENUM 값들 eintity에 String으로 저장되도록 설정 [#13]

--- src/main/java/org/jullaene/walkmong_back/api/member/domain/Member.java
+++ src/main/java/org/jullaene/walkmong_back/api/member/domain/Member.java
@@ -1,10 +1,6 @@
 package org.jullaene.walkmong_back.api.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,9 +36,11 @@ public class Member extends BaseEntity {
     private Integer birthDate;
 
     @Comment("성별")
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Comment("역할")
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Comment("자기소개")
