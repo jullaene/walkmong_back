a0fcd9c5c66fcc07d069a60f502710fd74e83d2e
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-16 12:40:59 +0900

    fix: 게시글 리스트 조회에 게시글 작성 시간 추가

--- src/main/java/org/jullaene/walkmong_back/api/board/dto/res/BoardResponseDto.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/dto/res/BoardResponseDto.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
 import org.jullaene.walkmong_back.common.enums.Gender;
 
+import java.time.LocalDateTime;
+
 public record BoardResponseDto(String startTime, String endTime, String matchingYn,
                                String dogName, String dogProfile,
                                Gender dogGender, String breed, Double weight,
                                DogSize dogSize, String content,
-                               String dongAddress, Double distance) {
+                               String dongAddress, Double distance, LocalDateTime createdAt) {
 
     @Builder
     public BoardResponseDto {
