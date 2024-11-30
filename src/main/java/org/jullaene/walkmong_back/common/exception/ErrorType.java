9423d283535ed2b62fdf587985feb9ad1a8d1272
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-30 19:09:36 +0900

    feat: 닉네임 중복 체크 로직 구현 [#21]

--- src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
+++ src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
@@ -12,6 +12,7 @@ public enum ErrorType {
     LOGIN_REQUIRED("로그인 해주세요."),
     EXPIRED_TOKEN("만료된 요청입니다."),
     ALREADY_EXIST_USER("이미 존재하는 유저입니다."),
+    ALREADY_EXIST_NICKNAME("이미 존재하는 닉네임입니다."),
     ALREADY_LOGIN("로그인 상태입니다"),
     WRONG_PASSWORD("잘못된 비밀번호 입니다."),
     REQUEST_VALIDATION_ERROR("유효성 검사가 실패하였습니다."),
