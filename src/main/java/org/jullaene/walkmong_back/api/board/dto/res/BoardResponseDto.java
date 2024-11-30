bc4f310d529093da79714bd37fa1596ba5a37819
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-11-30 14:06:06 +0900

    feat: 게시글 상세 정보 조회 api 구현

--- src/main/java/org/jullaene/walkmong_back/api/board/dto/res/BoardResponseDto.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/dto/res/BoardResponseDto.java
@@ -4,7 +4,8 @@ import lombok.Builder;
 import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
 import org.jullaene.walkmong_back.common.enums.Gender;
 
-public record BoardResponseDto(String startTime, String endTime, String matchingYn,
+
+public record BoardResponseDto(Long boardId, String startTime, String endTime, String matchingYn,
                                String dogName, String dogProfile,
                                Gender dogGender, String breed, Double weight,
                                DogSize dogSize, String content,
