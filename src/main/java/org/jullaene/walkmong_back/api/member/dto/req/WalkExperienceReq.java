ed9194756b0b88a2e96b6705660b727f3a5af67c
dnwldla <leeemjw@naver.com>
2024-11-30 19:21:16 +0900

    feat: 산책 경험 관련 추가 정보 등록

---
+++ src/main/java/org/jullaene/walkmong_back/api/member/dto/req/WalkExperienceReq.java
@@ -0,0 +1,12 @@
+package org.jullaene.walkmong_back.api.member.dto.req;
+
+import lombok.Getter;
+
+
+@Getter
+public class WalkExperienceReq {
+
+    private String dogOwnershipYn;
+    private String dogWalkingExperienceYn;
+    private String availabilityWithSize;
+}
