faadfba364ca37af2de4376b74b79ebe6cdff9f9
dnwldla <leeemjw@naver.com>
2024-12-02 15:09:02 +0900

    feat: 강아지 프로필 등록 기능 추가

--- src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
+++ src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
@@ -22,6 +22,7 @@ public enum ErrorType {
     INVALID_ADDRESS("유효하지 않은 주소입니다."),
     CANNOT_SELF_APPLY("본인의 게시글에는 지원할 수 없습니다."),
     CANNOT_DUPLICATED_APPLY("한 게시글에 여러 번 지원할 수 없습니다."),
+    CANNOT_DUPLICATED_DOG_PROFILE("이미 등록된 강아지입니다")
     ;
 
     private String message;
