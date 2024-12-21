76988ee0ab15e945dde31487d8ffc4de2e5c4a39
Seung Ik Lee <natwo98@gmail.com>
2024-12-21 16:12:54 +0900

    feat: #35 반려견 리스트 조회 API 구현

--- src/main/java/org/jullaene/walkmong_back/api/dog/rest/DogController.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/rest/DogController.java
@@ -1,6 +1,7 @@
 package org.jullaene.walkmong_back.api.dog.rest;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.jullaene.walkmong_back.api.dog.dto.req.DogProfileReqDto;
 import org.jullaene.walkmong_back.api.dog.dto.res.DogProfileResponseDto;
@@ -25,7 +26,6 @@ public class DogController {
     public ResponseEntity<BasicResponse<Long>> registerDogProfile(@Valid @RequestBody DogProfileReqDto dogProfileReqDto) {
             return ResponseEntity.ok(BasicResponse.ofSuccess(dogService.registerDogProfile(dogProfileReqDto)));
     }
-
     @PatchMapping("/profile/update/{dogId}")
     public ResponseEntity<BasicResponse<DogProfileResponseDto>> updateDogProfile(
             @PathVariable Long dogId,
@@ -34,4 +34,9 @@ public class DogController {
         return ResponseEntity.ok(BasicResponse.ofSuccess(updatedProfile));
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<BasicResponse<List<DogProfileResponseDto>>> getDogProfiles() {
+        List<DogProfileResponseDto> dogProfileList = dogService.getDogProfileList();
+        return ResponseEntity.ok(BasicResponse.ofSuccess(dogProfileList));
+    }
 }
