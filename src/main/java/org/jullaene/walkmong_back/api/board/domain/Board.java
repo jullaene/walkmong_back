483bf607810a41ea858fdd0cb50fe179d742d92d
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-12-21 20:54:59 +0900

    refactor: board 테이블에 ownerId 추가 -[#46]

--- src/main/java/org/jullaene/walkmong_back/api/board/domain/Board.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/domain/Board.java
@@ -3,22 +3,31 @@ package org.jullaene.walkmong_back.api.board.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicUpdate;
 import org.jullaene.walkmong_back.api.board.domain.enums.WalkingStatus;
+import org.jullaene.walkmong_back.api.board.dto.req.BoardRequestDto;
 import org.jullaene.walkmong_back.common.BaseEntity;
 
 @Table(name = "board")
 @Entity
+@NoArgsConstructor
 @DynamicUpdate
 public class Board extends BaseEntity {
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long boardId;
 
     @Comment("반려동물 아이디")
     private Long dogId;
+
+    @Comment("반려인 아이디")
+    private Long ownerId;
 
     @Comment("반려인 주소 아이디")
     private Long ownerAddressId;
@@ -31,13 +40,12 @@ public class Board extends BaseEntity {
     private String matchingYn;
 
     @Comment("산책 시작 시간")
+    @Column(name = "start_time", columnDefinition = "TIMESTAMP")
     private LocalDateTime startTime;
 
     @Comment("산책 종료 시간")
+    @Column(name = "end_time", columnDefinition = "TIMESTAMP")
     private LocalDateTime endTime;
-
-    @Comment("산책용품 제공 가능 여부")
-    private String suppliesProvideYn;
 
     @Comment("만남 장소 협의 여부")
     private String locationNegotiationYn;
@@ -48,5 +56,6 @@ public class Board extends BaseEntity {
     @Comment("산책 진행 여부")
     @Enumerated(EnumType.STRING)
     private WalkingStatus walkingStatus;
+
 
 }
