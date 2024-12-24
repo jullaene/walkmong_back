885907084b684d5dcafeb2d453837240c487d79f
dnwldla <leeemjw@naver.com>
2024-12-24 11:31:10 +0900

    feat: 최종 매칭 내역 확인하기

--- src/main/java/org/jullaene/walkmong_back/api/apply/rest/ApplyController.java
+++ src/main/java/org/jullaene/walkmong_back/api/apply/rest/ApplyController.java
@@ -1,7 +1,6 @@
 package org.jullaene.walkmong_back.api.apply.rest;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.jullaene.walkmong_back.api.apply.dto.req.ApplyRequestDto;
 import org.jullaene.walkmong_back.api.apply.dto.res.ApplyInfoDto;
 import org.jullaene.walkmong_back.api.apply.service.ApplyService;
@@ -9,7 +8,6 @@ import org.jullaene.walkmong_back.common.BasicResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/walking/apply")
@@ -30,5 +28,4 @@ public class ApplyController {
         ApplyInfoDto applyInfoDto=applyService.getApplyInfo(boardId);
         return ResponseEntity.ok(BasicResponse.ofSuccess(applyInfoDto));
     }
-
 }
