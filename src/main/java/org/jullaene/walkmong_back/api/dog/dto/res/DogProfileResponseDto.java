0aff1d3aeeb935a24d53f453617001b9e1739079
Seung Ik Lee <natwo98@gmail.com>
2024-12-24 13:42:55 +0900

    fix: #51 반려견 리스트 조회 API 수정

--- src/main/java/org/jullaene/walkmong_back/api/dog/dto/res/DogProfileResponseDto.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/dto/res/DogProfileResponseDto.java
@@ -2,6 +2,7 @@ package org.jullaene.walkmong_back.api.dog.dto.res;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.jullaene.walkmong_back.api.dog.domain.enums.DogSize;
 import org.jullaene.walkmong_back.common.enums.Gender;
 
 @Getter
@@ -9,6 +10,7 @@ import org.jullaene.walkmong_back.common.enums.Gender;
 public class DogProfileResponseDto {
     private Long dogId;
     private String dogName;
+    private DogSize dogSize;
     private String dogProfile;
     private Gender dogGender;
     private Integer dogAge;
