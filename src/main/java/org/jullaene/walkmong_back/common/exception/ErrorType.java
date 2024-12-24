a46adfa256e1905ce5b3e1632cfce4d41dac979b
dnwldla <leeemjw@naver.com>
2024-12-24 16:06:26 +0900

    feat: 최종 매칭 내역 확인하기

--- src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
+++ src/main/java/org/jullaene/walkmong_back/common/exception/ErrorType.java
@@ -25,6 +25,8 @@ public enum ErrorType {
     CANNOT_SELF_APPLY("본인의 게시글에는 지원할 수 없습니다."),
     CANNOT_DUPLICATED_APPLY("한 게시글에 여러 번 지원할 수 없습니다."),
     CANNOT_DUPLICATED_DOG_PROFILE("이미 등록된 강아지입니다"),
+    INVALID_FILE("존재하지 않는 파일입니다."),
+    INVALID_S3_FILE("AWS S3 저장소에 존재하지 않는 파일입니다."),
     APPLY_NOT_FOUND("존재하지 않는 지원입니다"),
     POST_NOT_FOUND("존재하지 않는 산책글입니다")
     ;
