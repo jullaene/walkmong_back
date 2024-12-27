3cd6018c4bbd65a4107b19389ef4f9c9ec90608c
이 은정 <jk2310400@ajou.ac.kr>
2024-12-28 03:14:23 +0900

    fix: 게시글 기능 수정

--- src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
+++ src/main/java/org/jullaene/walkmong_back/api/board/service/BoardService.java
@@ -2,6 +2,9 @@ package org.jullaene.walkmong_back.api.board.service;
 
 import lombok.RequiredArgsConstructor;
 import org.jullaene.walkmong_back.api.board.repository.BoardRepository;
+import org.jullaene.walkmong_back.common.exception.CustomException;
+import org.jullaene.walkmong_back.common.exception.ErrorType;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
