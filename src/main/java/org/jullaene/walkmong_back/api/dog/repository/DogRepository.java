2418d26d1e80a2559bd8e049571d3d0595dd0cfc
이 은정 <jk2310400@ajou.ac.kr>
2024-12-28 02:59:18 +0900

    fix: 임포트 추가

--- src/main/java/org/jullaene/walkmong_back/api/dog/repository/DogRepository.java
+++ src/main/java/org/jullaene/walkmong_back/api/dog/repository/DogRepository.java
@@ -4,6 +4,8 @@ import org.jullaene.walkmong_back.api.dog.domain.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DogRepository extends JpaRepository<Dog, Long> {
     Optional<Dog> findByDogIdAndDelYn(Long id, String delYn);
