885398f38a9da297c2d100fe16ee6694dab10b93
dnwldla <leeemjw@naver.com>
2024-12-29 16:41:13 +0900

    fix: 방에 입장할 때만 토큰을 검증하게 로직 수정 대화를 전송할 때는 토큰을 검증하지 않게 수정

--- src/main/java/org/jullaene/walkmong_back/config/ChatPreHandler.java
+++ src/main/java/org/jullaene/walkmong_back/config/ChatPreHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jullaene.walkmong_back.common.utils.JwtTokenUtil;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
@@ -18,14 +19,15 @@ public class ChatPreHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
-        // 헤더 토큰 얻기
-        String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
-        String token = authorizationHeader.substring(BEARER_PREFIX.length());
 
-        //토큰 검증
-        jwtTokenUtil.verify(token);
-        log.info("토큰 검증 완료");
-
+        //연결일 때만 토큰 검증하기
+        if (headerAccessor.getCommand()== StompCommand.CONNECT){
+            String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
+            String token = authorizationHeader.substring(BEARER_PREFIX.length());
+            //토큰 검증
+            jwtTokenUtil.verify(token);
+            log.info("토큰 검증 완료");
+        }
         return message;
 
     }
