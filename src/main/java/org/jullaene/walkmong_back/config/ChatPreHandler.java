83c674675465a94ebe158019a15678c6682042e1
dnwldla <leeemjw@naver.com>
2024-12-30 14:31:36 +0900

    feat: 헤더에 인증된 사용자를 추가하는 로직 추가

--- src/main/java/org/jullaene/walkmong_back/config/ChatPreHandler.java
+++ src/main/java/org/jullaene/walkmong_back/config/ChatPreHandler.java
@@ -2,12 +2,17 @@ package org.jullaene.walkmong_back.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jullaene.walkmong_back.api.member.service.MemberService;
+import org.jullaene.walkmong_back.common.jwt.JwtAuthenticationFilter;
 import org.jullaene.walkmong_back.common.utils.JwtTokenUtil;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -16,17 +21,18 @@ import org.springframework.stereotype.Component;
 public class ChatPreHandler implements ChannelInterceptor {
     private static final String BEARER_PREFIX = "Bearer ";
     private final JwtTokenUtil jwtTokenUtil;
+
+    //CONNECT,SUBSCRIBE,SEND 모두 거쳐간다
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor headerAccessor =  MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
         //연결일 때만 토큰 검증하기
-        if (headerAccessor.getCommand()== StompCommand.CONNECT){
+        if (headerAccessor.getCommand().equals(StompCommand.CONNECT) ){
             String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
             String token = authorizationHeader.substring(BEARER_PREFIX.length());
-            //토큰 검증
-            jwtTokenUtil.verify(token);
-            log.info("토큰 검증 완료");
+           headerAccessor.setUser(jwtTokenUtil.getAuthentication(token));
+            SecurityContextHolder.getContext().setAuthentication((Authentication) headerAccessor.getUser());
         }
         return message;
 
