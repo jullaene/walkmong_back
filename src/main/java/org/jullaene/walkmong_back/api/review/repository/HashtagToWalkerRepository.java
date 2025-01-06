6263f9f29aac4fb8d7bb6873979ee958c5837250
dnwldla <leeemjw@naver.com>
2025-01-06 17:44:56 +0900

    feat: 상위 3개 해시태그를 계산하는 로직 추가

--- src/main/java/org/jullaene/walkmong_back/api/review/repository/HashtagToWalkerRepository.java
+++ src/main/java/org/jullaene/walkmong_back/api/review/repository/HashtagToWalkerRepository.java
@@ -1,9 +1,11 @@
 package org.jullaene.walkmong_back.api.review.repository;
 
 import org.jullaene.walkmong_back.api.review.domain.HashtagToWalker;
+import org.jullaene.walkmong_back.api.review.dto.res.HashtagResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HashtagToWalkerRepository extends JpaRepository<HashtagToWalker, Long> {
+public interface HashtagToWalkerRepository extends JpaRepository<HashtagToWalker, Long>,HashtagCustomRepository {
+
 }
