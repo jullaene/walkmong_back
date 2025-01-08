7b6bcf7826fe764d1e74c209aa1ba9131dbcce49
이 은정 <jk2310400@ajou.ac.kr>
2025-01-09 03:51:28 +0900

    feat: 산책 후기 리ì스트 조회 (반려ã인 -> 산책자) api 구현

--- src/main/java/org/jullaene/walkmong_back/api/review/repository/ReviewToWalkerImageRepository.java
+++ src/main/java/org/jullaene/walkmong_back/api/review/repository/ReviewToWalkerImageRepository.java
@@ -3,5 +3,5 @@ package org.jullaene.walkmong_back.api.review.repository;
 import org.jullaene.walkmong_back.api.review.domain.ReviewToWalkerImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewToWalkerImageRepository extends JpaRepository<ReviewToWalkerImage, Long> {
+public interface ReviewToWalkerImageRepository extends JpaRepository<ReviewToWalkerImage, Long>, ReviewToWalkerImageRepositoryCustom {
 }
--- src/main/java/org/jullaene/walkmong_back/api/review/rest/ReviewToWalkerController.java
+++ src/main/java/org/jullaene/walkmong_back/api/review/rest/ReviewToWalkerController.java
@@ -1,15 +1,16 @@
 package org.jullaene.walkmong_back.api.review.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.jullaene.walkmong_back.api.review.dto.req.ReviewToWalkerReqDto;
+import org.jullaene.walkmong_back.api.review.dto.res.ReviewToWalkerRes;
 import org.jullaene.walkmong_back.api.review.service.ReviewToWalkerService;
 import org.jullaene.walkmong_back.common.BasicResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +21,13 @@ public class ReviewToWalkerController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<BasicResponse<Long>> registerReviewToOwner(@Valid @ModelAttribute ReviewToWalkerReqDto reviewToWalkerReqDto) {
+    public ResponseEntity<BasicResponse<Long>> registerReviewToWalker (@Valid @ModelAttribute ReviewToWalkerReqDto reviewToWalkerReqDto) {
         return ResponseEntity.ok(BasicResponse.ofSuccess(reviewToWalkerService.registerReviewToWalker(reviewToWalkerReqDto)));
+    }
+
+    @Operation(summary = "산책 후기 리스트 조회 (반려인 -> 산책자)", description = "반려인이 산책자에 대해 작성한 산책 후기 리스트를 조회하는 API입니다.")
+    @GetMapping("/list")
+    public ResponseEntity<BasicResponse<List<ReviewToWalkerRes>>> getReviewToWalkerList () {
+        return ResponseEntity.ok(BasicResponse.ofSuccess(reviewToWalkerService.getReviewToWalkerList()));
     }
 }
