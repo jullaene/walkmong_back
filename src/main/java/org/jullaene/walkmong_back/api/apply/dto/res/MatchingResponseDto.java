5a746a348feace9a85cfd9ac1704cd6df2bd2353
Seung Ik Lee <natwo98@gmail.com>
2025-01-16 22:32:45 +0900

    fix: #142 매칭 현황 리스트 조회 API 관련 수정

--- src/main/java/org/jullaene/walkmong_back/api/apply/dto/res/MatchingResponseDto.java
+++ src/main/java/org/jullaene/walkmong_back/api/apply/dto/res/MatchingResponseDto.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class MatchingResponseDto {
-    private final Long boardId;
     private final String tabStatus;
     private final String dogName;
     private final Gender dogGender;
@@ -21,10 +20,10 @@ public class MatchingResponseDto {
     private final String walkerName;
     private final String walkerProfile;
     private final String walkMatchingStatus;
+    private final Long boardId;
 
 
     public MatchingResponseDto() {
-        this.boardId = null;
         this.tabStatus = null;
         this.dogName = null;
         this.dogGender = null;
@@ -36,5 +35,6 @@ public class MatchingResponseDto {
         this.walkerName = null;
         this.walkerProfile = null;
         this.walkMatchingStatus = null;
+        this.boardId = null;
     }
 }
